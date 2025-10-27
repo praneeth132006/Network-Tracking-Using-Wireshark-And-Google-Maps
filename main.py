2671829805f688332db13efd1a92f82c9a0b5b4c
Praneeth <158541664+praneeth132006@users.noreply.github.com>
2025-10-27 13:09:48 +0530

    Add main function to process pcap file and generate KML

--- main.py
+++ main.py
@@ -40,3 +40,17 @@ def plotIPs(pcap):
         except:
             pass
     return kmlPts
+
+def main():
+    f = open('wire.pcap', 'rb')
+    pcap = dpkt.pcap.Reader(f)
+    kmlheader = '<?xml version="1.0" encoding="UTF-8"?> \n<kml xmlns="http://www.opengis.net/kml/2.2">\n<Document>\n'\
+    '<Style id="transBluePoly">' \
+                '<LineStyle>' \
+                '<width>1.5</width>' \
+                '<color>501400E6</color>' \
+                '</LineStyle>' \
+                '</Style>'
+    kmlfooter = '</Document>\n</kml>\n'
+    kmldoc=kmlheader+plotIPs(pcap)+kmlfooter
+    print(kmldoc)
